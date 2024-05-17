350dedb7a7a08f560889a6fc495d6ffebaac9bd7
twonumberfortyfives <sma4no.7@gmail.com>
2024-05-17 12:33:42 +0300

    Solution

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from user.views import CreateUserView, CreateTokenView, ManageUserView
 
-app_name = "user"
+from .views import CreateUserView, LoginUserView, ManageUserView
+
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
-    path("login/", CreateTokenView.as_view(), name="login"),
+    path("login/", LoginUserView.as_view(), name="login"),
     path("me/", ManageUserView.as_view(), name="manage"),
 ]
+
+app_name = "user"
